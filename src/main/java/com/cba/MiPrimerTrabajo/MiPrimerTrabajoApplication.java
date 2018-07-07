3fb36fd983119f0ad59ad6298ed02651f80b7f8e
Julia1297 <valita129@gmail.com>
2018-07-06 20:43:35 -0400

    First commit

---
+++ src/main/java/com/cba/MiPrimerTrabajo/MiPrimerTrabajoApplication.java
@@ -0,0 +1,12 @@
+package com.cba.MiPrimerTrabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerTrabajoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cba/MiPrimerTrabajo/MiPrimerTrabajoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cba.MiPrimerTrabajo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiPrimerTrabajoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
