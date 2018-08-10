38de63c5b81758e9807551a1365853fc07268480
IvySaskia <saskiaivy@outlook.com>
2018-08-10 00:48:14 -0400

    jueves10/08/2018

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,6 +21,7 @@ insert into company(id,name) values (1,'Comteco');
 insert into company(id,name) values (2,'Gobierno Autónomo Municipal');
 insert into company(id,name) values (3,'Jalasoft');
 insert into company(id,name) values (4,'Alcaldia Municipal de Cochabamaba');
+insert into company(id,name) values (5,'Instituto Boliviano De Metrologia - IBMETRO');
 
 insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name,knowledge)
 values (1,2,1,3,'Desarrolladores de Java con pasion por la programacion y tecnologia','3 años o mas','Ingenieria de Sistemas  o Licenciatura en Informatica','Ingles intermedio','Desarrolladores',"Java");
@@ -58,3 +59,25 @@ values (15,1,1,4,'Proponer y ejecutar políticas, programas, proyectos, activida
 
 insert into link(id,name,description,link,category_id) values (1, 'Codecademy','Un portal donde puedes aprender diversos lenguajes de programacion.','https://www.codecademy.com/es',2);
 insert into link(id,name,description,link,category_id) values (2, 'Fao','El aprendizaje en línea para satisfacer las necesidades de los profesionales de la agricultura y la seguridad alimentaria','http://www.fao.org',3);
+
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (16,5,1,5,'Representar y dirigir el proceso de gestión, planificación, organización, integración, control y evaluación del proceso administrativo, operativo, financiero, técnico e institucional de los servicios que ofrece la Regional Cochabamba','3 años','Profesional con Título en Provisión Nacional en Ingeniería, Licenciatura en Física o ramas afines.','Inglés','JEFE REGIONAL COCHABAMBA');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (17,1,1,5,'Coadyuvar, asistir y apoyar a jefatura regional y personal dependiente, en las labores de secretariado, ejecutándolas dentro de un marco de alta confiabilidad, responsabilidad, discreción y eficiencia. ','2 años','Secretaria(o) Ejecutiva (deseable), Comercial o Administrativa','Ingles','SECRETARIA (O) JEFATURA');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (18,1,1,5,'Ejecutar y controlar las operaciones contables y administrativas específicas a nivel Institucional rigiéndose en las normas de contabilidad gubernamental y las leyes vigentes.','1 años','Licenciado en Auditoria, Administración de empresas, Contaduría Pública, Ing. Comercial y/o Industrial o ramas afines','Ingles','RESPONSABLE ADMINISTRATIVO');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (19,5,1,5,'Ejercer control de la calidad del servicio que prestan los técnicos de la oficina regional, controlar el buen uso de los equipos y verificar la calibración y verificación de los instrumentos de medición','2 años','Ingeniero Mecánico, Electrónico, Industrial, Petrolero, Químico, Licenciatura en Física o ramas afines.','Dos idiomas oficiales del Estado Plurinacional de Bolivia,Ingles','SUPERVISOR');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (20,5,1,5,'Realizar óptimamente el control metrológico de instrumentos de medición involucrados en transacciones comerciales, salud y medio ambiente de acuerdo a procedimientos y reglamentos técnicos de Metrología Legal.','1 años','Mínimamente técnico Superior o Medio en Mecánica, Electrónica, Industrial, Petrolero, Química.','Dos idiomas oficiales del Estado Plurinacional de Bolivia.','TÉCNICO METROLOGO');
+
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (21,1,2,5,'Representar y dirigir el proceso de gestión, planificación, organización, integración, control y evaluación del proceso administrativo, operativo, financiero, técnico e institucional de los servicios que ofrece la Regional','5 años','Profesional con Título en Provisión Nacional en Ciencias Económicas, Administrativa, Financiera o Ingeniería, Licenciatura en Física o ramas afines,Maestría en temas afines al cargo (deseable)','Dos idiomas oficiales del Estado Plurinacional de Bolivia.,Ingles(Deseable)','JEFE REGIONAL');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (22,1,2,5,'Coadyuvar, asistir y apoyar a Jefatura Regional y personal dependiente, en las labores de secretariado, ejecutándolas dentro de un marco de alta confiabilidad, responsabilidad, discreción y eficiencia.','2 años','Secretaria(o) Ejecutiva (deseable), Comercial o Administrativa.','Dos idiomas oficiales del Estado Plurinacional de Bolivia,Idioma Ingles (deseable).','SECRETARIA (O) JEFATURA REGIONAL');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (23,5,2,5,'Ejercer control de la calidad del servicio que prestan los técnicos de la oficina regional, controlar el buen uso de los equipos y verificar la calibración y verificación de los instrumentos de medición involucrados',' años','Ingeniero Mecánico, Electrónico, Industrial, Petrolero, Químico, Licenciatura en Física o ramas afines',' Dos idiomas oficiales del Estado Plurinacional de Bolivia.,Idioma Ingles (Deseable).','SUPERVISOR');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (24,4,2,5,'Administrar y optimizar el uso de los Recursos Humanos, Materiales, Económicos y Financieros asignados a la Regional y coadyuvar la labor del Jefe Regional en el cumplimiento de objetivos','5 años','Egresado o Titulado en Administración de Empresas, Economía, Auditoría, Contaduría Pública, Ingeniero Industrial y/o Comercial.','Dos idiomas oficiales del Estado Plurinacional de Bolivia.','RESPONSABLE ADMINISTRATIVO');
+insert into requirements(id, category_id,city_id,company_id,description, experience, academic,languages,name)
+values (25,5,2,5,'Realizar óptimamente el control metrológico de instrumentos de medición involucrados en transacciones comerciales, salud y medio ambiente.','1 años','Técnico Superior o Medio en Mecánica, Electrónica, Industrial, Petrolero, Química','Dos idiomas oficiales del estado Plurinacional de Bolivia,Idioma Ingles (deseable).','TÉCNICO METROLOGO');
